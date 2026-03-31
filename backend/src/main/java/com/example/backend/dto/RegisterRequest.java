f74478bee0d3d67a46b6739253c28edb5fe4c55a
cerrium <aceren903@gmail.com>
2026-03-31 22:34:43 +0300

    added frontend and backend, updated gitignore

---
+++ backend/src/main/java/com/example/backend/dto/RegisterRequest.java
@@ -0,0 +1,62 @@
+package com.example.backend.dto;
+
+public class RegisterRequest {
+
+    private String role;
+    private String fullName;
+    private String email;
+    private String password;
+    private String location;
+    private String phone;
+
+    public RegisterRequest() {
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
